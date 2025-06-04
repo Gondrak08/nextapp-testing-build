e0a731b187d1afc966bb44258d6174e636fc26ec
=vitor <vitor.alecrim.p@gmail.com>
2025-06-04 15:07:46 -0300

    next.config.js fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const { redirect } = require("next/dist/server/api-utils");
-const path = require("path");
+// const { redirect } = require("next/dist/server/api-utils");
+// const path = require("path");
 
-module.exports = (phase, { defaultConfig }) => {
-  if ("sassOptions" in defaultConfig) {
-    defaultConfig["sassOptions"] = {
-      includePaths: [path.join(__dirname, "styles")],
-      prependData: `@import "~@/app/styles/variables.scss";`,
-    };
-  }
-  return {
-    ...defaultConfig,
-    output: "export",
-    distDir: "dist",
-  };
+const nextConfig = {
+  output: "export", // Para exportação estática (obrigatório para Netlify)
+  distDir: "dist", // Opcional - pasta de output personalizada
+  images: {
+    unoptimized: true, // Necessário quando usando 'output: export'
+  },
+  sassOptions: {
+    includePaths: [require("path").join(__dirname, "styles")],
+    prependData: `@import "~@/app/styles/variables.scss";`,
+  },
+  // Outras configurações específicas do seu projeto...
+  trailingSlash: true, // Recomendado para export estático
 };
+module.exports = nextConfig;
+// module.exports = (phase, { defaultConfig }) => {
+//   if ("sassOptions" in defaultConfig) {
+//     defaultConfig["sassOptions"] = {
+//       includePaths: [path.join(__dirname, "styles")],
+//       prependData: `@import "~@/app/styles/variables.scss";`,
+//     };
+//   }
+//   return {
+//     ...defaultConfig,
+//     output: "export",
+//     distDir: "dist",
+//   };
+// };
